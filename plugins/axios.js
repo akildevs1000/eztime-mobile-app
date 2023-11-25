f532af90b3c48f8a96755a9741f85f2a90d16e35
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-11-25 16:23:44 +0400

    added

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,13 +2,13 @@ export default ({ $axios, store }, inject) => {
   // Add an interceptor to modify requests globally
   $axios.onRequest((config) => {
     // Append the branchid parameter to all requests
-    let user = store.state.auth.user;
-    if (user && user.employee) {
-      config.params = {
-        ...config.params,
-        branch_id: user && user.employee && user.employee.branch_id,
-      };
-    }
+    // let user = store.state.auth.user;
+    // if (user && user.employee) {
+    //   config.params = {
+    //     ...config.params,
+    //     branch_id: user && user.employee && user.employee.branch_id,
+    //   };
+    // }
     // config.cache = {
     //   maxAge: 10 * 1000, // Set maxAge to 30 minutes if the condition is met
     // };
