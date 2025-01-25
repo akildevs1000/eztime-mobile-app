cca330de800c8a2b9dac14637a668dd3e8120531
akildevs1000 <akildevs1000@gmail.com>
2025-01-25 14:09:22 +0400

    TEST

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,13 @@
 export default ({ $axios, store }, inject) => {
   // Add an interceptor to modify requests globally
   $axios.onRequest(async (config) => {
-    config.baseURL = `http://${window.location.hostname ?? "localhost"}:8000/api`;
+
+    // for DESKTOP
+    // config.baseURL = `http://${window.location.hostname ?? "localhost"}:8002/api`;
+
+    // for CLOUD
+    config.baseURL = `https://backend.mytime2cloud.com/api`;
+
     // Append the branchid parameter to all requests
     let user = store.state.auth.user;
 
